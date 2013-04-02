6f33c2cc297f0c91028da9313cb8fa7d74605d4d
Chang Hu <changhu@live.com>
2013-04-02 01:09:51 -0700

    fapi deployments

---
+++ FapiViz/FapiViz/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FapiViz.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/Csv
+        public ContentResult Csv()
+        {
+            var handler = new HttpClientHandler
+                {
+                    UseDefaultCredentials = true
+                };
+            var httpClient = new HttpClient(handler)
+                           {
+                               BaseAddress = new Uri("http://ctdri/")
+                           };
+
+            var result = httpClient.GetStringAsync("FlightDeployment?excel=1").Result;
+            const string fileName = "file";
+
+            HttpContext.Response.AddHeader("Content-Disposition", string.Format("attachment; filename=\"{0}.csv\"", fileName));
+            return new ContentResult()
+                {
+                    Content = result, 
+                    ContentType = string.Format("text/csv; name=\"{0}.csv\"", fileName)
+                };
+        }
+
+    }
+}
---
+++ FapiViz/FapiViz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FapiViz.MvcApplication" Language="C#" %>
